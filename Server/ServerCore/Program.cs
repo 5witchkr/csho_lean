798f56a0008cc71f56b5844facdf924a819dc11a
5witchkr <hob4419@gmail.com>
2022-12-29 01:51:17 +0900

    thread basic

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+namespace ServerCore;
 
+class Program
+{
+    static void MainThread()
+    {
+        Console.WriteLine("Main Thread Run!!!");
+    }
+    static void Main(String[] args)
+    {
+        Thread t = new Thread(MainThread);
+
+        t.Start();
+
+        Console.WriteLine("Hello World");
+
+    }
+}
